a150003d4adebc712eb68273eaaffbeb22cc7945
Nicollas1305 <cartola1320@gmail.com>
2025-01-24 11:49:21 -0300

    first commit :)

---
+++ script.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import json
+
+chromedriver_path = "/Users/nicollasmarinho/Downloads/chromedriver-mac-x64/chromedriver"
+chrome_profile_path = "/Users/nicollasmarinho/Biblioteca/Application Support/Google/Chrome/Default"
+
+options = webdriver.ChromeOptions()
+options.add_argument(f"user-data-dir={chrome_profile_path}")
+
+service = Service(chromedriver_path)
+driver = webdriver.Chrome(service=service, options=options)
+
+post_url = "https://www.instagram.com/p/DFLDoyRuTnH/"
+driver.get(post_url)
+time.sleep(5)
+
+with open('followers.json', 'r') as file:
+    seguidores = json.load(file)
+
+usernames = [
+    follower['string_list_data'][0]['value']
+    for follower in seguidores
+]
+
+for username in usernames:
+    print("Procurando o campo de comentário...")
+    try:
+        comment_input = WebDriverWait(driver, 1).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "textarea"))
+        )
+        comment_input.click()
+        comment_input.send_keys(f"@{username}")
+
+        publish_button = WebDriverWait(driver, 1).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@role='button' and text()='Publicar']"))
+        )
+        publish_button.click()
+
+        print(f"Comentário feito: @{username}")
+    except Exception as e:
+        print(f"Erro ao comentar para @{username}: {e}")
+    time.sleep(30)
+
+driver.quit()
